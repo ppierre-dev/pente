9449548fb44630a6186ab4a90960a0a2062c1846
LucasDoutreluigne <49163072+LucasDoutreluigne@users.noreply.github.com>
2021-05-25 09:04:16 +0200

    Delete env_dev directory

--- env_dev/Couleur.java
+++
@@ -1,9 +0,0 @@
-/**
- * Énumération Couleur
- * Représente la couleur d'un joueur pour identifier
- * ses pions et le tour de jeu
- */
-public enum Couleur {
-    NOIR,
-    BLANC
-}
--- env_dev/README.md
+++
@@ -1,1 +0,0 @@
-# Environnement de développement
