379e3dc2b3763f8ac20f919a7b87898cf2378f50
Hiro Hito <etienne.ogez@gmail.com>
2021-06-04 14:50:31 +0200

    04/06/21 14H50 ia

--- src/IA.java
+++ src/IA.java
@@ -25,4 +25,46 @@ public class IA extends Joueur{
         return position;
     }
 
+    public Position calculerCoupniveau2(){
+        Position position;
+        Position positiontest;
+        Integer suite;
+        suite = 1;
+        int place;
+        position = new Position(0,0);
+        boolean libre;
+        for (int x=0; x<19;x++) {
+            for (int y=0; y<19;y++) {
+                position = new Position(x,y);
+                libre = Jeu.getInstancePrincipale().getPlateau().estLibre(position);
+                if(libre == false && Jeu.getInstancePrincipale().getPlateau().getIntersection(position) != this.getCouleur()){
+                    for(int a=x; a< a + 3; a++){
+                        positiontest = new Position(a,y);
+                        if(Jeu.getInstancePrincipale().getPlateau().estLibre(positiontest) == false && Jeu.getInstancePrincipale().getPlateau().getIntersection(position) != this.getCouleur()){
+                            suite++;
+                            if(suite == 4){
+                                place = 1;
+                            }
+                        }
+                    }
+                    suite = 1;
+                    for(int a=x; a > a - 3; a--){
+                        positiontest = new Position(a,y);
+                        if(Jeu.getInstancePrincipale().getPlateau().estLibre(positiontest) == false && Jeu.getInstancePrincipale().getPlateau().getIntersection(position) != this.getCouleur()){
+                            suite = suite + 1;
+                            if(suite == 4){
+                                place = 1;
+                            }
+                        }
+                    }
+
+
+                    }
+                }
+            }
+        }
+        return position;
+        this.getCouleur()
+    }
+
 }
